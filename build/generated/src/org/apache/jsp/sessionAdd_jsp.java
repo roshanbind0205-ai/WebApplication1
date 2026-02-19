27bd2b806b4a3ca0ad328c8c3a7186251b04ac19
roshanbind0205-ai <roshanbind0205@gmail.com>
2026-02-19 16:35:52 +0530

    update

--- build/generated/src/org/apache/jsp/sessionAdd_jsp.java
+++ build/generated/src/org/apache/jsp/sessionAdd_jsp.java
@@ -49,7 +49,7 @@ public final class sessionAdd_jsp extends org.apache.jasper.runtime.HttpJspBase
       out.write("    <head>\n");
       out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
       out.write("        <title>Login Page</title>\n");
-      out.write("        \n");
+      out.write("\n");
       out.write("        <style>\n");
       out.write("            input{\n");
       out.write("                width:300px;\n");
@@ -64,9 +64,9 @@ public final class sessionAdd_jsp extends org.apache.jasper.runtime.HttpJspBase
       out.write("                box-shadow: 0px 0px 7px;\n");
       out.write("                background-color:highlighttext;\n");
       out.write("            }\n");
-      out.write("            \n");
+      out.write("\n");
       out.write("        </style>\n");
-      out.write("        \n");
+      out.write("\n");
       out.write("    </head>\n");
       out.write("    <body>\n");
       out.write("        ");
@@ -75,43 +75,41 @@ session.setAttribute("name", "Roshan");
       out.write("        <div>\n");
       out.write("            <center>\n");
       out.write("                <form method=\"post\">\n");
-      out.write("            <h2>Login Page</h2>\n");
-      out.write("            <h4>Enter access password to continue</h4>\n");
-      out.write("            <input type=\"text\" name=\"user_name\" placeholder=\"user_name\"/>\n");
-      out.write("            <br><br>\n");
-      out.write("            <input type=\"text\"  name=\"password\" placeholder=\"password\"/>\n");
-      out.write("            <br><br>\n");
-      out.write("            <button type=\"Submit\">Submit</button>\n");
-      out.write("            </form>\n");
+      out.write("                    <h2>Login Page</h2>\n");
+      out.write("                    <h4>Enter access password to continue</h4>\n");
+      out.write("                    <input type=\"text\" name=\"user_name\" placeholder=\"user_name\"/>\n");
+      out.write("                    <br><br>\n");
+      out.write("                    <input type=\"text\"  name=\"password\" placeholder=\"password\"/>\n");
+      out.write("                    <br><br>\n");
+      out.write("                    <button type=\"Submit\">Submit</button>\n");
+      out.write("                </form>\n");
       out.write("            </center>\n");
       out.write("        </div>\n");
-      out.write("        \n");
+      out.write("\n");
       out.write("        ");
 
-             String user_name = request.getParameter("user_name");
-             String password = request.getParameter("password");
-             
-              
-            try{
-            
-           java.sql.Connection connection = Data.Connector.getConnection();
-           java.sql.PreparedStatement statement = connection.prepareStatement("select * from login where user_name=? AND password=?");           
-           
-           statement.setString(1, user_name);
-           statement.setString(2 ,password);
-           java.sql.ResultSet rs= statement.executeQuery();
-           
-           if(rs.next()){
-               out.println("correct");
-               session.setAttribute("user_name",user_name);
-               session.setAttribute("password", password);
-           }
-           else{
-               out.println("incorrect");
-           }
-        } catch (Exception e) {
-          
-        }           
+            String user_name = request.getParameter("user_name");
+            String password = request.getParameter("password");
+
+            try {
+
+                java.sql.Connection connection = Data.Connector.getConnection();
+                java.sql.PreparedStatement statement = connection.prepareStatement("select * from login where user_name=? AND password=?");
+
+                statement.setString(1, user_name);
+                statement.setString(2, password);
+                java.sql.ResultSet rs = statement.executeQuery();
+
+                if (rs.next()) {
+                    out.println("correct");
+                    session.setAttribute("user_name", user_name);
+                    session.setAttribute("password", password);
+                } else {
+                    out.println("incorrect");
+                }
+            } catch (Exception e) {
+
+            }
         
       out.write("\n");
       out.write("    </body>\n");
--- build/web/sessionAdd.jsp
+++ build/web/sessionAdd.jsp
@@ -10,7 +10,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Login Page</title>
-        
+
         <style>
             input{
                 width:300px;
@@ -25,51 +25,49 @@
                 box-shadow: 0px 0px 7px;
                 background-color:highlighttext;
             }
-            
+
         </style>
-        
+
     </head>
     <body>
         <%session.setAttribute("name", "Roshan");%>
         <div>
             <center>
                 <form method="post">
-            <h2>Login Page</h2>
-            <h4>Enter access password to continue</h4>
-            <input type="text" name="user_name" placeholder="user_name"/>
-            <br><br>
-            <input type="text"  name="password" placeholder="password"/>
-            <br><br>
-            <button type="Submit">Submit</button>
-            </form>
+                    <h2>Login Page</h2>
+                    <h4>Enter access password to continue</h4>
+                    <input type="text" name="user_name" placeholder="user_name"/>
+                    <br><br>
+                    <input type="text"  name="password" placeholder="password"/>
+                    <br><br>
+                    <button type="Submit">Submit</button>
+                </form>
             </center>
         </div>
-        
+
         <%
-             String user_name = request.getParameter("user_name");
-             String password = request.getParameter("password");
-             
-              
-            try{
-            
-           java.sql.Connection connection = Data.Connector.getConnection();
-           java.sql.PreparedStatement statement = connection.prepareStatement("select * from login where user_name=? AND password=?");           
-           
-           statement.setString(1, user_name);
-           statement.setString(2 ,password);
-           java.sql.ResultSet rs= statement.executeQuery();
-           
-           if(rs.next()){
-               out.println("correct");
-               session.setAttribute("user_name",user_name);
-               session.setAttribute("password", password);
-           }
-           else{
-               out.println("incorrect");
-           }
-        } catch (Exception e) {
-          
-        }           
+            String user_name = request.getParameter("user_name");
+            String password = request.getParameter("password");
+
+            try {
+
+                java.sql.Connection connection = Data.Connector.getConnection();
+                java.sql.PreparedStatement statement = connection.prepareStatement("select * from login where user_name=? AND password=?");
+
+                statement.setString(1, user_name);
+                statement.setString(2, password);
+                java.sql.ResultSet rs = statement.executeQuery();
+
+                if (rs.next()) {
+                    out.println("correct");
+                    session.setAttribute("user_name", user_name);
+                    session.setAttribute("password", password);
+                } else {
+                    out.println("incorrect");
+                }
+            } catch (Exception e) {
+
+            }
         %>
     </body>
 </html>
